12253ac7af48e25d9c6694763c65679c937b3351
Joachim Jensen (BIT BLUEPRINT) <jj@bitblueprint.com>
2014-02-25 12:31:03 +0100

    chaos filters in proper columns. filter icons displayed properly

--- wp-content/themes/dka/templates/chaos-search-form.php
+++ wp-content/themes/dka/templates/chaos-search-form.php
@@ -60,29 +60,34 @@ $advanced_search_expanded = ((/*!empty($types) ||*/ !empty($organizations)) ? " 
 	<div id="advanced-search-container" class="col-xs-12 collapse<?php echo $advanced_search_expanded; ?>">
 
 		<div class="col-xs-12 filter-container filter-media-type visible-xs">
-			<label class="btn filter-btn filter-btn-all"><?php _e('All Formats','dka'); ?><i class="icon-ok"></i></label>
+			<div class="col-xs-12">
+				<label class="btn filter-btn filter-btn-all"><i class="icon-ok"></i><?php _e('All Formats','dka'); ?></label>
+			</div>
+			
 <?php foreach(WPDKAObject::$format_types as $format_type => $args) : if($format_type == WPDKAObject::TYPE_IMAGE_AUDIO || $format_type == WPDKAObject::TYPE_UNKNOWN) continue; ?>
-			<div class="col-xs-12" style="padding-right: 2px;">
+			<div class="col-xs-12">
 				<label title="<?php echo $args['title']; ?>" for="<?php echo WPDKASearch::QUERY_KEY_TYPE .'-'. $format_type; ?>" class="btn filter-btn filter-btn-single">
-					<input type="checkbox" class="chaos-filter" style="display: none;" name="<?php echo WPDKASearch::QUERY_KEY_TYPE; ?>[]" value="<?php echo $format_type; ?>" id="<?php echo WPDKASearch::QUERY_KEY_TYPE .'-'. $format_type; ?>" <?php checked(in_array($format_type,(array)$types)); ?>>
-					<i class="<?php echo $args['class']; ?>"></i><?php echo $args['title']; ?> (<?php echo get_facet_count(WPDKASearch::QUERY_KEY_TYPE, $args['chaos-value']) ?>)<i class="icon-remove-sign"></i>
+					<input type="checkbox" class="chaos-filter" name="<?php echo WPDKASearch::QUERY_KEY_TYPE; ?>[]" value="<?php echo $format_type; ?>" id="<?php echo WPDKASearch::QUERY_KEY_TYPE .'-'. $format_type; ?>" <?php checked(in_array($format_type,(array)$types)); ?>>
+					<i class="icon-remove-sign"></i><i class="<?php echo $args['class']; ?>"></i><?php echo $args['title']; ?> (<?php echo get_facet_count(WPDKASearch::QUERY_KEY_TYPE, $args['chaos-value']) ?>)
 				</label>
 			</div>
 <?php endforeach; ?>
 		</div>
 
 		<div class="col-xs-12 col-sm-12 filter-container filter-organizations">
-			<label class="btn filter-btn filter-btn-all col-sm-12"><?php _e('All Organizations','dka'); ?><i class="icon-ok"></i></label>
+			<div class="col-xs-12">
+				<label class="btn filter-btn filter-btn-all col-xs-12"><i class="icon-ok"></i><?php _e('All Organizations','dka'); ?></label>
+			</div>
 <?php
 $current_organization_id = 0;
 foreach(WPDKASearch::get_organizations_merged() as $id => $organization) :
 	$count = get_facet_count(WPDKASearch::QUERY_KEY_ORGANIZATION, $organization['chaos_titles']);
 
 ?>
-			<div class="col-md-6 col-sm-6 col-lg-4 col-xs-12" style="padding-right: 2px;">
-				<label for="<?php echo WPDKASearch::QUERY_KEY_ORGANIZATION .'-'. $organization['slug']; ?>" class="btn filter-btn filter-btn-single col-md-6i col-sm-6i col-lg-4i col-xs-12">
-					<input type="checkbox" class="chaos-filter" style="display: none;" name="<?php echo WPDKASearch::QUERY_KEY_ORGANIZATION; ?>[]" value="<?php echo $organization['slug']; ?>" id="<?php echo WPDKASearch::QUERY_KEY_ORGANIZATION .'-'. $organization['slug']; ?>" <?php checked(in_array($organization['slug'],(array)$organizations)); ?>>
-					<?php echo $organization['title']; ?> (<?php echo $count ?>)<i class="icon-remove-sign"></i>
+			<div class="col-md-6 col-sm-6 col-lg-4 col-xs-12">
+				<label for="<?php echo WPDKASearch::QUERY_KEY_ORGANIZATION .'-'. $organization['slug']; ?>" class="btn filter-btn filter-btn-single">
+					<input type="checkbox" class="chaos-filter" name="<?php echo WPDKASearch::QUERY_KEY_ORGANIZATION; ?>[]" value="<?php echo $organization['slug']; ?>" id="<?php echo WPDKASearch::QUERY_KEY_ORGANIZATION .'-'. $organization['slug']; ?>" <?php checked(in_array($organization['slug'],(array)$organizations)); ?>>
+					<i class="icon-remove-sign"></i><?php echo $organization['title']; ?> (<?php echo $count ?>)
 				</label> 
 			</div>
 <?php endforeach; ?>
