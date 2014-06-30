73828120b86941aba2d85280a1ccaac024f4cbe4
madslundt <madssx@hotmail.com>
2014-06-30 12:46:39 +0200

    Ignoring objects with title 'uden titel' and 'ukendt titel'

--- wp-content/plugins/wpdka/wpdkasearch.php
+++ wp-content/plugins/wpdka/wpdkasearch.php
@@ -43,6 +43,9 @@ class WPDKASearch {
 		),
 	);
 
+	// Words to be ignored when searching for objects.
+	public static $search_words_ignore = array("!uden titel", "!ukendt titel");
+
 	/**
 	 * Construct
 	 */
@@ -122,7 +125,6 @@ class WPDKASearch {
 			}
 				
 			$query[] = '(ObjectTypeID:(' . implode(" OR ", WPDKAObject::$OBJECT_TYPE_IDS) . '))';
-				
 			return implode(" AND ", $query);
 		}, 9, 2);
 		
@@ -145,6 +147,9 @@ class WPDKASearch {
 				foreach(WPDKAObject::$FREETEXT_SCHEMA_GUIDS as $schemaGUID) {
 					foreach(WPDKAObject::$FREETEXT_LANGUAGE as $language) {
 						$searches[] = sprintf("(m%s_%s_all:(%s))", $schemaGUID, $language, $freetext);
+
+						// Ignoring objects.
+						$searches[] = sprintf("(m%s_%s_all:(%s))", $schemaGUID, $language, implode(' AND ', self::$search_words_ignore));
 					}
 				}
 				$query[] = '(' . implode(" OR ", $searches) . ')';
