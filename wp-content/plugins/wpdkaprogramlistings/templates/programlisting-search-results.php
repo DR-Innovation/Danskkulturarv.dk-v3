e6ded4fe603740beafb55d134c59c76731182d49
Simon Carstensen <me@simonbc.com>
2015-08-06 11:51:32 +0200

    Move schedule free-text search to top

--- wp-content/plugins/wpdkaprogramlistings/templates/programlisting-search-results.php
+++ wp-content/plugins/wpdkaprogramlistings/templates/programlisting-search-results.php
@@ -3,28 +3,11 @@
     $results = WPDKAProgramListings::get_programlisting_results(); 
     $search_text = WPDKAProgramListings::get_programlisting_search_type() === WPDKAProgramListings::QUERY_KEY_FREETEXT;
 ?>
-<div class="schedule-free-text-search row">
-    <div class="js-free-text-search-content">
-        <form method="GET" action="<?php echo get_permalink(get_option('wpdkaprogramlistings-page')); ?>">
-            <div class="col-xs-12 col-lg-6 col-sm-9">
-                <div class="input-group">
-                    <input type="text" name="<?php echo WPDKAProgramListings::QUERY_KEY_FREETEXT; ?>" class="form-control programlistings-search-text" placeholder="<?php _e('Search program schedule', WPDKAProgramListings::DOMAIN); ?>" value="<?php echo WPDKAProgramListings::get_programlisting_var(WPDKAProgramListings::QUERY_KEY_FREETEXT, 'esc_attr,trim'); ?>" />
-                    <div class="input-group-addon hover-info" data-html="true" data-container="body" data-toggle="popover" data-placement="bottom" data-trigger="hover" data-content="<?php WPDKAProgramListings::print_search_info_text(); ?>">
-                        <i class="icon icon-info-sign"></i>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xs-12 col-lg-2 col-sm-3" style="padding-bottom: 5px; padding-left: 0;">
-                <button type="submit" class="btn btn-primary btn-search btn-block" id="searchsubmit"><?php _e('Search', WPDKAProgramListings::DOMAIN); ?></button>
-            </div>
-        </form>
-    </div>
-</div>
 <div class="programlisting-search-results row">
     <div class="programlisting-count col-lg-3 col-md-9 col-sm-9 col-xs-10">
         <img src="<?php echo plugins_url( '../images/logo.png' , __FILE__ ); ?>" alt="TV og Radio" style="vertical-align: top; max-width: 100%; max-height: 38px;" />
     </div>
-    <div class="js-date-search-content<?php echo $search_text ? ' hidden' : ''; ?>">
+    <div class="js-date-search-content">
     	<form method="GET" action="<?php echo get_permalink(get_option('wpdkaprogramlistings-page')); ?>">
     		<div class="col-xs-12 col-md-3 col-sm-3 col-lg-2">
     			<div class="programlisting-year">
@@ -62,7 +45,7 @@
     	</form>
     </div>
     <noscript>
-        <form method="GET" class="<?php echo !$search_text ? 'hidden' : ''; ?>" action="<?php echo get_permalink(get_option('wpdkaprogramlistings-page')); ?>">
+        <form method="GET" action="<?php echo get_permalink(get_option('wpdkaprogramlistings-page')); ?>">
             <div class="col-xs-12 col-md-3 col-sm-3 col-lg-2">
                 <div class="programlisting-year">
                     <select name="<?php echo WPDKAProgramListings::QUERY_KEY_YEAR; ?>">
@@ -97,7 +80,7 @@
                 <button type="submit" class="btn btn-primary btn-block"><?php _e('Search on date', WPDKAProgramListings::DOMAIN); ?></button>
             </div>
         </form>
-        <form class="free-text-search<?php echo $search_text ? ' hidden' : ''; ?>" method="GET" action="<?php echo get_permalink(get_option('wpdkaprogramlistings-page')); ?>">
+        <form class="free-text-search>" method="GET" action="<?php echo get_permalink(get_option('wpdkaprogramlistings-page')); ?>">
             <div class="col-xs-12 col-lg-6 col-sm-9 col-lg-offset-3 col-md-offset-0">
                 <input type="text" class="programlistings-search-text" name="<?php echo WPDKAProgramListings::QUERY_KEY_FREETEXT; ?>" class="form-control" placeholder="<?php _e('Search in program listings', WPDKAProgramListings::DOMAIN); ?>" value="<?php echo WPDKAProgramListings::get_programlisting_var(WPDKAProgramListings::QUERY_KEY_FREETEXT, 'esc_attr,trim'); ?>" />
             </div>
