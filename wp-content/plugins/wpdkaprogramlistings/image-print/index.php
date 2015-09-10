b08aa9bb4ab4f9d909c51290df05deaae33218f9
Andreas Larsen <andreas@larsenwork.com>
2015-09-10 09:39:21 +0200

    programlistings: opdateret sprogfil

--- wp-content/plugins/wpdkaprogramlistings/image-print/index.php
+++ wp-content/plugins/wpdkaprogramlistings/image-print/index.php
@@ -58,7 +58,11 @@ $social = '
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <?php if ($type == 'card') : ?>
   <title>Postkort Dansk Kulturarv</title>
+  <?php elseif ($type == 'poster') : ?>
+  <title>Plakat Dansk Kulturarv</title>
+  <?php endif; ?>
 
   <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
