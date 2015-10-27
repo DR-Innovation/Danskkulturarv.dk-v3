a5812fec521c6b9d680c14be683ff0df44099d86
Andreas Larsen <andreas@larsenwork.com>
2015-10-27 17:33:23 +0100

    clean search results

--- wp-content/themes/dka/templates/chaos-search-results.php
+++ wp-content/themes/dka/templates/chaos-search-results.php
@@ -11,20 +11,20 @@ $current_sort = isset(WPDKASearch::$sorts[WPChaosSearch::get_search_var(WPChaosS
 $only_published_objects = WPChaosSearch::get_search_var(WPChaosSearch::QUERY_KEY_ONLY_PUBLISHED) == 'publicerede';
 
 $views = array(
-	array(
-		'title' => __('View as List','wpchaossearch'),
-		'view' => 'listview',
-		'class' => 'icon-th-list',
-		'link' => 'liste'
-		),
-	array(
-		'title' => __('View as Gallery','wpchaossearch'),
-		'view' => 'thumbnails',
-		'class' => 'icon-th',
-		'link' => null
-		),
-	);
-	?>
+  array(
+    'title' => __('View as List','wpchaossearch'),
+    'view' => 'listview',
+    'class' => 'icon-th-list',
+    'link' => 'liste'
+    ),
+  array(
+    'title' => __('View as Gallery','wpchaossearch'),
+    'view' => 'thumbnails',
+    'class' => 'icon-th',
+    'link' => null
+    ),
+  );
+  ?>
 
 <div class="container body-container">
 
@@ -32,109 +32,100 @@ $views = array(
 <div class="search row"><?php dynamic_sidebar( 'Top' ); ?></div>
 <!-- end search -->
 
-	<article class="search-results">
-		<div class="row search-results-top">
-			<div class="col-md-4 col-sm-12 col-xs-12 search-count">
-				<?php echo $result_count = sprintf(__('The search gave %s results for %s','dka'),number_format_i18n(WPChaosSearch::get_search_results()->MCM()->TotalCount()),'<strong class="blue">'.WPChaosSearch::get_search_var(WPChaosSearch::QUERY_KEY_FREETEXT, 'esc_html').'</strong>'); ?>
-			</div>
-			<div class="col-md-4 col-sm-6 col-xs-12 search-result-listing">
-				<div class="btn-group">
-				 	<button type="button" class="btn btn-sm btn-default dropdown-toggle" data-toggle="dropdown"><?php _e('Sort by:','dka'); ?> <strong class="blue"><?php echo $current_sort; ?></strong>
-				 		<i class="icon-caret-down"></i>
-				 	</button>
-				 	<ul class="dropdown-menu">
-					<?php foreach(WPDKASearch::$sorts as $sort) : ?>
-						<li><a tabindex="-1" href="<?php echo WPChaosSearch::generate_pretty_search_url(array(WPChaosSearch::QUERY_KEY_SORT => $sort['link'], WPChaosSearch::QUERY_KEY_PAGE => null)); ?>" title="<?php echo $sort['title']; ?>"><?php echo $sort['title']; ?></a></li>
-					<?php endforeach; ?>
-				 	</ul>
-				</div>&nbsp;
-				<div class="btn-group">
-					<?php foreach($views as $view) :
-					echo '<a type="button" class="btn btn-default btn-sm'.($view['view'] == $current_view ? ' active' : '').'" href="'.WPChaosSearch::generate_pretty_search_url(array(WPChaosSearch::QUERY_KEY_VIEW => $view['link'])).'" title="'.$view['title'].'"><i class="'.$view['class'].'"></i></a>';
-					endforeach; ?>
-				</div>
-				<?php if (current_user_can(WPDKA::PUBLISH_STATE_CAPABILITY)): ?>
-					<div class="btn-group">
-						<a type="button" class="btn btn-default btn-sm<?php echo $only_published_objects ? ' active' : ''; ?>" title="<?php _e('Show only published objects', 'dka'); ?>" href="<?php echo WPChaosSearch::generate_pretty_search_url(array(WPChaosSearch::QUERY_KEY_ONLY_PUBLISHED => ($only_published_objects ? '' : 'publicerede'))); ?>"><i class="icon-eye-open"></i></a>
-					</div>
-				<?php endif; ?>
-			</div>
-			<div class="col-md-4 col-sm-6 col-xs-12 pagination-div">
-				<ul class="pagination">
-					<?php echo $pagination = WPChaosSearch::paginate('echo=0&before=&after=&count=5'); ?>
-				</ul>
-			</div>
-		</div>
-	<ul class="row <?php echo $current_view; ?>">
+  <article class="search-results">
+    <div class="row search-results-top">
+      <div class="col-md-4 col-sm-12 col-xs-12 search-count">
+        <?php echo $result_count = sprintf(__('The search gave %s results for %s','dka'),number_format_i18n(WPChaosSearch::get_search_results()->MCM()->TotalCount()),'<strong class="blue">'.WPChaosSearch::get_search_var(WPChaosSearch::QUERY_KEY_FREETEXT, 'esc_html').'</strong>'); ?>
+      </div>
+      <div class="col-md-4 col-sm-6 col-xs-12 search-result-listing">
+        <div class="btn-group">
+           <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown"><?php _e('Sort by:','dka'); ?> <span class="blue"><?php echo $current_sort; ?></span>
+             <i class="icon-caret-down"></i>
+           </button>
+           <ul class="dropdown-menu">
+          <?php foreach(WPDKASearch::$sorts as $sort) : ?>
+            <li><a tabindex="-1" href="<?php echo WPChaosSearch::generate_pretty_search_url(array(WPChaosSearch::QUERY_KEY_SORT => $sort['link'], WPChaosSearch::QUERY_KEY_PAGE => null)); ?>" title="<?php echo $sort['title']; ?>"><?php echo $sort['title']; ?></a></li>
+          <?php endforeach; ?>
+           </ul>
+        </div>&nbsp;
+        <div class="btn-group">
+          <?php foreach($views as $view) :
+          echo '<a type="button" class="btn btn-default'.($view['view'] == $current_view ? ' active' : '').'" href="'.WPChaosSearch::generate_pretty_search_url(array(WPChaosSearch::QUERY_KEY_VIEW => $view['link'])).'" title="'.$view['title'].'"><i class="'.$view['class'].'"></i></a>';
+          endforeach; ?>
+        </div>
+        <?php if (current_user_can(WPDKA::PUBLISH_STATE_CAPABILITY)): ?>
+          <div class="btn-group">
+            <a type="button" class="btn btn-default <?php echo $only_published_objects ? ' active' : ''; ?>" title="<?php _e('Show only published objects', 'dka'); ?>" href="<?php echo WPChaosSearch::generate_pretty_search_url(array(WPChaosSearch::QUERY_KEY_ONLY_PUBLISHED => ($only_published_objects ? '' : 'publicerede'))); ?>"><i class="icon-eye-open"></i></a>
+          </div>
+        <?php endif; ?>
+      </div>
+    </div>
+  <ul class="row <?php echo $current_view; ?>">
 
-		<?php
-		//Consider using a action/filter to print single result
-		foreach(WPChaosSearch::get_search_results()->MCM()->Results() as $object) :
-			$collection_obj = null;
-			if(class_exists('WPDKACollections') && $object->ObjectTypeID == WPDKACollections::COLLECTIONS_TYPE_ID) :
-				$collection_obj = new WPChaosObject($object,WPDKACollections::OBJECT_FILTER_PREFIX);
-				//Safety. Should never happen
-				if(!isset(WPDKACollections::$collection_relations[$object->GUID])) {
-					continue;
-				}
-				$object = WPDKACollections::$collection_relations[$object->GUID];
+    <?php
+    //Consider using a action/filter to print single result
+    foreach(WPChaosSearch::get_search_results()->MCM()->Results() as $object) :
+      $collection_obj = null;
+      if(class_exists('WPDKACollections') && $object->ObjectTypeID == WPDKACollections::COLLECTIONS_TYPE_ID) :
+        $collection_obj = new WPChaosObject($object,WPDKACollections::OBJECT_FILTER_PREFIX);
+        //Safety. Should never happen
+        if(!isset(WPDKACollections::$collection_relations[$object->GUID])) {
+          continue;
+        }
+        $object = WPDKACollections::$collection_relations[$object->GUID];
 
-			endif;
-			WPChaosClient::set_object($object);
-			$thumbnail = (WPChaosClient::get_object()->thumbnail ? ' style="background-image: url(\''.WPChaosClient::get_object()->thumbnail.'\')!important;"' : '');
-			$url = WPChaosClient::get_object()->url;
-			$caption = WPChaosClient::get_object()->caption;
-			$title = WPChaosClient::get_object()->title;
-			$organization = WPChaosClient::get_object()->organization;
-			$views = number_format_i18n((double)WPChaosClient::get_object()->views);
-			$class = '';
-			$publish = current_user_can(WPDKA::PUBLISH_STATE_CAPABILITY) ? WPChaosClient::get_object()->isPublished ? '' : ' unpublished' : '';
-			if($collection_obj) {
-				$url .= '#'.$collection_obj->GUID;
-				$caption = count($collection_obj->ObjectRelations) . ' ' . _n( 'material', 'materials', count($collection_obj->ObjectRelations), 'dka' );
-				$title = $collection_obj->title;
-				$organization = "Samling";
-				$views = null;
-				$class = ' collection-result';
-			}
-			?>
-			<li class="search-object col-xs-12 col-sm-6 col-lg-3<?php echo $class ?><?php echo $publish; ?>">
-				<a class="thumbnail" href="<?php echo $url; ?>" id="<?php echo WPChaosClient::get_object()->GUID; ?>">
+      endif;
+      WPChaosClient::set_object($object);
+      $thumbnail = (WPChaosClient::get_object()->thumbnail ? ' style="background-image: url(\''.WPChaosClient::get_object()->thumbnail.'\')!important;"' : '');
+      $url = WPChaosClient::get_object()->url;
+      $caption = WPChaosClient::get_object()->caption;
+      $title = WPChaosClient::get_object()->title;
+      $organization = WPChaosClient::get_object()->organization;
+      $views = number_format_i18n((double)WPChaosClient::get_object()->views);
+      $class = '';
+      $publish = current_user_can(WPDKA::PUBLISH_STATE_CAPABILITY) ? WPChaosClient::get_object()->isPublished ? '' : ' unpublished' : '';
+      if($collection_obj) {
+        $url .= '#'.$collection_obj->GUID;
+        $caption = count($collection_obj->ObjectRelations) . ' ' . _n( 'material', 'materials', count($collection_obj->ObjectRelations), 'dka' );
+        $title = $collection_obj->title;
+        $organization = "Samling";
+        $views = null;
+        $class = ' collection-result';
+      }
+      ?>
+      <li class="search-object col-xs-12 col-sm-6 col-lg-3<?php echo $class ?><?php echo $publish; ?>">
+        <a class="thumbnail" href="<?php echo $url; ?>" id="<?php echo WPChaosClient::get_object()->GUID; ?>">
 
-					<div class="thumb format-<?php echo WPChaosClient::get_object()->type; ?>"<?php echo $thumbnail; ?>>
-						<?php  if($caption):?>
-						<div class="caption"><?php echo $caption ?></div>
-					<?php endif;?>
-					<?php if(class_exists('WPDKACollections') && current_user_can(WPDKACollections::CAPABILITY) && !$collection_obj) : ?>
-						<button type="button" class="add-to-collection btn"><span class="icon-plus"></span></button>
-					<?php endif; ?>
-					</div>
-					<h2 class="title"><strong><?php echo $title; ?></strong></h2>
-					<p class="organization orange"><strong><?php echo $organization; ?></strong></p>
-					<?php if(WPChaosClient::get_object()->published && $collection_obj == null) : ?>
-						<p class="date"><i class="icon-calendar"></i> <?php echo WPChaosClient::get_object()->published; ?></p>
-					<?php endif; ?>
-					<hr>
-					<div class="media-type-container">
-						<i title="<?php echo WPChaosClient::get_object()->type_title; ?>" class="<?php echo WPChaosClient::get_object()->type_class; ?>"></i>
-						<?php if(0 && $views) : ?>
-						<i class="icon-eye-open"> <?php echo $views; ?></i>
-						<?php endif; ?>
-					</div>
-				</a>
-			</li>
+          <div class="thumb format-<?php echo WPChaosClient::get_object()->type; ?>"<?php echo $thumbnail; ?>>
+            <?php  if($caption):?>
+            <div class="caption"><?php echo $caption ?></div>
+          <?php endif;?>
+          <?php if(class_exists('WPDKACollections') && current_user_can(WPDKACollections::CAPABILITY) && !$collection_obj) : ?>
+            <button type="button" class="add-to-collection btn"><span class="icon-plus"></span></button>
+          <?php endif; ?>
+          </div>
+          <h2 class="title"><strong><?php echo $title; ?></strong></h2>
+          <p class="organization orange"><strong><?php echo $organization; ?></strong></p>
+          <?php if(WPChaosClient::get_object()->published && $collection_obj == null) : ?>
+            <p class="date"><?php echo WPChaosClient::get_object()->published; ?></p>
+          <?php endif; ?>
+          <div class="media-type-container">
+            <i title="<?php echo WPChaosClient::get_object()->type_title; ?>" class="<?php echo WPChaosClient::get_object()->type_class; ?>"></i>
+            <?php if(0 && $views) : ?>
+            <i class="icon-eye-open"> <?php echo $views; ?></i>
+            <?php endif; ?>
+          </div>
+        </a>
+      </li>
 <?php endforeach; WPChaosClient::reset_object(); ?>
 </ul>
 
-<div class="row search-results-top search-results-bottom">
-	<div class="col-sm-6 hidden-xs search-count">
-		<?php echo $result_count; ?>
-	</div>
-	<div class="col-sm-6 col-xs-12 pagination-div">
-		<ul class="pagination">
-			<?php echo $pagination; ?>
-		</ul>
-	</div>
+<div class="row">
+  <div class="col-xs-12 text-center">
+    <ul class="pagination">
+      <?php echo $pagination = WPChaosSearch::paginate('echo=0&before=&after=&count=5'); ?>
+    </ul>
+  </div>
 </div>
 </article>
 
