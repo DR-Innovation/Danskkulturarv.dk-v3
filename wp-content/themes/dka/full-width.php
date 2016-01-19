1204840ac9865d941b9771a86cb510d164d6aaa5
Andreas Larsen <andreas@larsenwork.com>
2016-01-19 16:05:58 +0100

    no title on iframe pages

--- wp-content/themes/dka/full-width.php
+++ wp-content/themes/dka/full-width.php
@@ -4,7 +4,7 @@
  * @subpackage DKA
  */
 /*
-Template Name: 100% bred
+Template Name: 100% iframes
 */
 add_action( 'wp_enqueue_scripts', function() {
 	wp_enqueue_style( 'full-width-style', get_stylesheet_directory_uri().'/css/full-width.css' );
@@ -14,10 +14,6 @@ get_header(); ?>
 <?php while ( have_posts() ) : the_post(); ?>
   <div class="row">
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-      <h1 class="entry-title">
-        <?php the_title(); ?>
-      </h1>
-
       <div class="entry-content">
         <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'dka' ) ); ?>
         <?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'dka' ), 'after' => '</div>' ) ); ?>
