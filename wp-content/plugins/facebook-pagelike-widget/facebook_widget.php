e06cf7aa13a4681e68d1ce0cee41d28a9849e60a
Andreas Larsen <andreas@larsenwork.com>
2016-06-09 09:55:45 +0200

    plugin updates

--- wp-content/plugins/facebook-pagelike-widget/facebook_widget.php
+++ wp-content/plugins/facebook-pagelike-widget/facebook_widget.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package Facebook Widget
- * @version 4.0
+ * @version 4.1
  */
 /*
 Plugin Name: Facebook Widget
 Plugin URI: http://patelmilap.wordpress.com/
 Description: This widget adds a Simple Facebook Page Like Widget into your wordpress website sidebar within few minutes.
 Author: Milap Patel
-Version: 4.0
+Version: 4.1
 Author URI: http://patelmilap.wordpress.com/
 Text Domain: facebook-pagelike-widget
 */
